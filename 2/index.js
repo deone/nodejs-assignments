feabad1c9143c5d8040a9043717daa6f01c327c1
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-01-15 07:44:22 +0000

    Users post handler.

--- 2/index.js
+++ 2/index.js
@@ -1,49 +1,69 @@
 const http = require('http')
 const StringDecoder = require('string_decoder').StringDecoder
+const url = require('url')
 
 const { port, env } = require('./lib/config')
 const handlers = require('./lib/handlers')
 const helpers = require('./lib/helpers')
-const _data = require('./lib/data')
 
 
 // Testing
 // @TODO: Delete this
-//_data.create('test', 'newFile', {'foo': 'bar'})
-// _data.read('test', 'newFile')
+// const _data = require('./lib/data')
+
+/* _data.create('test', 'newFile', {'foo': 'bar'}, (err) => {
+  console.log('this was the error', err)
+}) */
+// _data.read('test', 'newFile', () => {})
 // _data.update('test', 'newFile', 'Aloha')
 // _data.delete('test', 'newFile')
 
 
 // Configure the server to respond to all requests with a string
 const server = http.createServer((req, res) => {
-
-  // Get path request is sent to
-  const trimmedPath = helpers.trimPath(req.url)
-
   // To process this request, we need a handler called thus:
   // handler(data, callback)
 
-  // Get handler
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
+  //Get the headers as an object
+  const headers = req.headers;
+
+  // -- Callback
+  const callback = (statusCode, message) => {
+    // set a default status code
+    statusCode = typeof statusCode === 'number' ? statusCode : 200
+
+    // Use the payload returned from the handler, or set the default payload to an empty object
+    message = typeof message == 'object'? message : {}
+
+    // Convert the payload to a string
+    const messageString = JSON.stringify(message);
+
+    // Return the response
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(statusCode)
+    res.end(messageString)
+    console.log(trimmedPath, statusCode)
+  }
+
+  // -- Handler
   // Use notFound handler if we can't find a handler
   const chosenHandler =
     typeof router[trimmedPath] !== 'undefined'
       ? router[trimmedPath]
       : handlers.notFound
-
-  // Create callback
-  const callback = (statusCode, message) => {
-    // set a default status code
-    statusCode = typeof statusCode === 'number' ? statusCode : 200
-
-    // create the JSON payload
-    const payload = { message }
-
-    // send a JSON response with the status code and the given message
-    res.setHeader('Content-Type', 'application/json')
-    res.writeHead(statusCode)
-    res.end(JSON.stringify(payload))
-  }
 
   // read in data provided by user into variable buffer
   const decoder = new StringDecoder('utf-8')
@@ -55,8 +75,18 @@ const server = http.createServer((req, res) => {
   req.on('end', () => {
     buffer += decoder.end()
 
+    // Construct the data object to send to the handler
+    // -- Data
+    const data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : helpers.parseJsonToObject(buffer)
+    };
+
     // handle request
-    chosenHandler(buffer, callback)
+    chosenHandler(data, callback)
   })
 })
 
--- 2/lib/handlers.js
+++ 2/lib/handlers.js
@@ -1,6 +1,66 @@
+/* Request Handlers */
+
+// Dependencies
+const path = require('path')
+
+const helpers = require('./helpers')
+
+
 const handlers = {}
 
 handlers.notFound = (data, callback) => callback(404, 'Not Found')
-handlers.users = (data, callback) => callback(200, 'Take a cookie!')
+
+handlers.users = (data, callback) => {
+  const acceptableMethods = ['post', 'get', 'put', 'delete']
+  if (acceptableMethods.includes(data.method)) {
+    handlers._users[data.method](data, callback)
+  } else {
+    callback(405)
+  }
+}
+
+handlers._users = {}
+
+// Users - post
+// Required data: firstName, lastName, email, streetAddress
+// Option data: none
+handlers._users.post = (data, callback) => {
+  // Check that all required fields are filled out
+  // firstName
+  const firstName = helpers.validate(data.payload.firstName)
+
+  // lastName
+  const lastName = helpers.validate(data.payload.lastName)
+
+  // email
+  const email = helpers.validate(data.payload.email)
+
+  // streetAddress
+  const streetAddress = helpers.validate(data.payload.streetAddress)
+
+  if(firstName && lastName && email && streetAddress) {
+    
+  } else {
+    callback(400, {'Error': 'Missing required fields'})
+  }
+}
+
+// Users - get
+// Required data: email
+// Optional data: none
+// @TODO Only let an authenticated user access their object. Dont let them access anyone elses.
+handlers._users.get = (data, callback) => {
+  // Validate email - do this properly, maybe with regex
+  const email = helpers.validate(data.queryStringObject.email.trim())
+  if(email) {
+    // Look up user
+  }
+}
+
+// Users - put
+handlers._users.put = (data, callback) => {}
+
+// Users - delete
+handlers._users.delete = (data, callback) => {}
 
 module.exports = handlers
