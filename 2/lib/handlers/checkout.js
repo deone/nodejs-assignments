b3b002d920b060d67b208896717cea90510c639e
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-02-19 15:47:15 +0000

    Return success message.

--- 2/lib/handlers/checkout.js
+++ 2/lib/handlers/checkout.js
@@ -19,6 +19,14 @@ checkoutHandler._checkout = {}
 
 // Checkout - post
 // Required data - token ID. Stripe token and order ID in payload
+/*
+  Get required data
+  Check that token is valid
+  Get user email
+  Get order
+  Send payment request to stripe
+  Send email to user
+*/
 checkoutHandler._checkout.post = (data, callBack) => {
   // Get token from header
   const tokenId = helpers.validate(data.headers.token)
@@ -31,7 +39,7 @@ checkoutHandler._checkout.post = (data, callBack) => {
       .then(token => {
         const tokenObject = helpers.parseJsonToObject(token)
         if (tokenObject.expires > Date.now()) {
-          // Get user
+          // Get user email
           helpers.readDir(helpers.filePath(helpers.baseDir, 'users'))
             .then(fileNames => {
               if (!fileNames.length) {
@@ -41,51 +49,58 @@ checkoutHandler._checkout.post = (data, callBack) => {
                 fileNames.forEach(fileName => {
                   const email = fileName.slice(0, -5)
                   if (email === tokenObject.email) {
-                    // Get order - we can get this from this user object
-                    // since it has a list of order objects.
+                    // Get order
                     helpers.readFile(helpers.filePath(helpers.baseDir,
-                      'users', email), 'utf8')
-                      .then(user => {
-                        const userObject = helpers.parseJsonToObject(user)
-                        userObject.orders.forEach(order => {
-                          if (order.id === orderId) {
-                            const stripeRequestObject = {
-                              amount: Math.round(order.value * 100),
-                              currency: 'usd',
-                              description: `${email}_${tokenId}_${Date.now()}`,
-                              source: stripeToken
-                            }
-                            const requestString = queryString.stringify(stripeRequestObject)
+                      'orders', orderId), 'utf8')
+                      .then(order => {
+                        const orderObject = helpers.parseJsonToObject(order)
+                        console.log(orderObject)
+                        const stripeRequestObject = {
+                          amount: Math.round(orderObject.totalPrice * 100),
+                          currency: 'usd',
+                          description: `${email}_${tokenId}_${Date.now()}`,
+                          source: stripeToken
+                        }
+                        const requestString = queryString.stringify(stripeRequestObject)
 
-                            const options = {
-                              hostname: 'api.stripe.com',
-                              port: 443,
-                              path: '/v1/charges',
-                              method: 'POST',
-                              headers: {
-                                'Authorization': `Bearer ${config.stripeKey}`,
-                                'Content-Type': 'application/x-www-form-urlencoded',
-                                'Content-Length': Buffer.byteLength(requestString)
-                              }
-                            }
-                          
-                            const req = https.request(options, res => {
-                              console.log(`statusCode: ${res.statusCode}`)
-                            
-                              res.on('data', d => {
-                                console.log(`${d}`)
-                                callBack(res.statusCode, `${d}`)
-                              })
-                            })
-                            
-                            req.on('error', error => {
-                              console.error(error)
-                            })
-                            
-                            req.write(requestString)
-                            req.end()
+                        const options = {
+                          hostname: 'api.stripe.com',
+                          port: 443,
+                          path: '/v1/charges',
+                          method: 'POST',
+                          headers: {
+                            'Authorization': `Bearer ${config.stripeKey}`,
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Content-Length': Buffer.byteLength(requestString)
                           }
+                        }
+
+                        const req = https.request(options, res => {
+                          console.log(`Stripe payment status code: ${res.statusCode}`)
+                        
+                          res.on('data', d => {
+                            console.log(`${d}`)
+                            // Update order object
+                            if (res.statusCode === 200) {
+                              orderObject.paid = true
+                              helpers.openFile(helpers.filePath(helpers.baseDir,
+                                'orders', orderId), 'w')
+                                .then(fileDescriptor => {
+                                  helpers.writeFile(fileDescriptor, JSON.stringify(orderObject))
+                                    .catch(err => callBack(500, {'Error': err.toString()}))
+                                  callBack(200, {'Success': 'Payment processed successfully.'})
+                                })
+                                .catch(err => callBack(500, {'Error': err.toString()}))
+                            }
+                          })
                         })
+                        
+                        req.on('error', error => {
+                          console.error(error)
+                        })
+                        
+                        req.write(requestString)
+                        req.end()
                       })
                       .catch(err => callBack(500, {'Error': err.toString()}))
                   }
