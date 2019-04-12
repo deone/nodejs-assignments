3cafd877e217d34b8ef178cf67e53d171bac37f6
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-04-12 14:36:36 +0000

    Handle menu items in a functional way. Return early in case of invalid input. Shorten code lines.

--- 3/lib/handlers/menu.js
+++ 3/lib/handlers/menu.js
@@ -5,11 +5,9 @@ const helpers = require('../helpers')
 
 const menuHandler = {}
 
-menuHandler.menu = (data, callback) => {
-  const acceptableMethods = ['get']
-  return helpers.requestDispatcher(
-    data, callback, acceptableMethods, menuHandler._menu)
-}
+menuHandler.menu = (data, callback) =>
+  helpers.requestDispatcher(
+    data, callback, ['get'], menuHandler._menu)
 
 menuHandler._menu = {}
 
@@ -19,46 +17,59 @@ menuHandler._menu = {}
 menuHandler._menu.get = (data, callBack) => {
   // Get tokenId from header
   const [tokenId] = helpers.validate(data.headers.token)
-  if (tokenId) {
-    // Get token
-    helpers.getToken(tokenId)
-      .then(token => {
-        const tokenObject = helpers.parseJsonToObject(token)
-        // Check whether token is valid
-        if (tokenObject.expires > Date.now()) {
-          // Token is valid
-          // Read menuitems directory
-          helpers.readDir(helpers.filePath(helpers.baseDir, 'menuitems'))
-            .then(fileNames => {
-              if (!fileNames.length) {
-                // There are no menu items
-                callBack(200, {'Message': 'There are no items on the menu.'})
-              } else {
-                // There are menu items
-                let menuItems = []
-                fileNames.forEach(fileName => {
-                  // Get menu item from each file
-                  helpers.readFile(
-                    helpers.filePath(helpers.baseDir, 'menuitems', fileName.slice(0, -5)), 'utf8')
-                    .then(menuItem => {
-                      menuItems.push(helpers.parseJsonToObject(menuItem))
-                      if (menuItems.length === fileNames.length) {
-                        callBack(200, menuItems)
-                      }
-                    })
-                    .catch(err => callBack(500, {'Error': err.toString()}))
-                })
-              }
-            })
-            .catch(err => callBack(500, {'Error': err.toString()}))
-        } else {
-          callBack(401, {'Error': 'Invalid token. Please log in again.'})
-        }
-      })
-      .catch(err => callBack(500, {'Error': err.toString()}))
-  } else {
+
+  if (!tokenId) {
     callBack(401, {'Error': 'Authentication token not provided.'})
+    return
   }
+
+  // Get token
+  helpers.getToken(tokenId)
+    .then(token => {
+      const tokenObject = helpers.parseJsonToObject(token)
+
+      // Check whether token is valid
+      if (!tokenObject.expires > Date.now()) {
+        callBack(401, {'Error': 'Invalid token. Please login again.'})
+        return
+      }
+
+      // Token is valid
+      // Read menuitems directory
+      helpers.readDir(helpers.filePath(helpers.baseDir, 'menuitems'))
+        .then(fileNames => {
+          if (!fileNames.length) {
+            // There are no menu items
+            callBack(200, {'Message': 'There are no items on the menu.'})
+            return
+          }
+
+          // There are menu items
+          const promises = fileNames.map(fileName => {
+            // Get menu item from each file
+            return helpers.readFile(
+                    helpers.filePath(
+                      helpers.baseDir,
+                      'menuitems',
+                      fileName.slice(0, -5)
+                    ),
+                    'utf8'
+                  )
+                    .then(menuItem =>
+                      helpers.parseJsonToObject(menuItem))
+                    .catch(err => callBack(500, {
+                      'Error': err.toString()
+                    }))
+          })
+
+          Promise.all(promises).then(menuItems => {
+            callBack(200, menuItems)
+          })
+
+        })
+        .catch(err => callBack(500, {'Error': err.toString()}))
+    })
+    .catch(err => callBack(500, {'Error': err.toString()}))
 }
 
 
