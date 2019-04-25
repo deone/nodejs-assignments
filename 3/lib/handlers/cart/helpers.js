ab9ff383dc5e5321a81a74d37e8a343c1a78b451
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-04-25 15:46:44 +0000

    Separate cart creation.

--- 3/lib/handlers/cart/helpers.js
+++ 3/lib/handlers/cart/helpers.js
@@ -3,20 +3,25 @@ const utils = require('../../utils')
 
 const helpers = {}
 
+helpers.createCart = user =>
+  callBack => {
+    user.cart = []
+    // Write updated object
+    const write = utils.fileWriter(user)
+    utils.openFile(
+      utils.userDir(user.email),
+      'w'
+    )
+      .then(write)
+      .catch(err =>
+        callBack(500, {'Error': err.toString()})
+      )
+  }
+
 helpers.getOrCreateCart = user =>
   callBack => {
     if (!user.hasOwnProperty('cart')) {
-      user.cart = []
-      // Write updated object
-      const write = utils.fileWriter(user)
-      utils.openFile(
-        utils.userDir(user.email),
-        'w'
-      )
-        .then(write)
-        .catch(err =>
-          callBack(500, {'Error': err.toString()})
-        )
+      helpers.createCart(user)(callBack)
     }
     callBack(200, user.cart)
   }
--- 3/lib/utils.js
+++ 3/lib/utils.js
@@ -212,24 +212,4 @@ utils.getTemplate = (templateName, data, callBack) => {
     : callBack('A valid template name was not specified.');
 }
 
-/* utils.getTemplate = function(templateName,data,callback){
-  templateName = typeof(templateName) == 'string' && templateName.length > 0 ? templateName : false;
-  data = typeof(data) == 'object' && data !== null ? data : {};
-  if(templateName){
-    var templatesDir = path.join(__dirname,'/../templates/');
-    fs.readFile(templatesDir+templateName+'.html', 'utf8', function(err,str){
-      if(!err && str && str.length > 0){
-        // Do interpolation on the string
-        var finalString = utils.interpolate(str,data);
-        callback(false,finalString);
-      } else {
-        callback('No template could be found');
-      }
-    });
-  } else {
-    callback('A valid template name was not specified');
-  }
-}; */
-
-
 module.exports = utils
