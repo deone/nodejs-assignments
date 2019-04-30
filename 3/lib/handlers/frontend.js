90328616e6071e8da1e7d1f74aae6db219be9819
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-04-30 15:26:05 +0000

    Pass arguments in right order.

--- 3/lib/handlers/frontend.js
+++ 3/lib/handlers/frontend.js
@@ -5,14 +5,15 @@ const { io } = require('../utils')
 
 const handlers = {}
 
-handlers.index = (data, callBack) =>
-  data.method === 'get'
-    ? io.getTemplate('index', data, callBack)
-        .then(string => {
-          callBack(200, string, 'html')
-        })
-        .catch(err => callBack(500, undefined, 'html'))
-    : callBack(405, undefined, 'html')
+handlers.index = callBack =>
+  data =>
+    data.method === 'get'
+      ? io.getTemplate('index', data, callBack)
+          .then(string => {
+            callBack(200, string, 'html')
+          })
+          .catch(err => callBack(500, undefined, 'html'))
+      : callBack(405, undefined, 'html')
 
 // Export the handlers
 module.exports = handlers
