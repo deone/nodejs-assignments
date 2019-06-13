013b11064615820b170ec4a02efd855b673ee791
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-06-13 03:57:15 +0000

    Test checkout API

--- 3/test/api.js
+++ 3/test/api.js
@@ -67,7 +67,6 @@ api['/api/logout should return success message'] = done => {
   // Create token
   const callBack = () => console.log('hello')
   const token = crypto.createToken(callBack)('a@a.com')(crypto.createRandomString(20))
-  console.log()
 
   const payLoad = JSON.stringify({})
 
@@ -266,11 +265,11 @@ api["PUT /api/cart should update and return user's cart"] = done => {
   const callBack = () => console.log('hello')
   const token = crypto.createToken(callBack)('h@a.com')(crypto.createRandomString(20))
 
-  const data = JSON.stringify({
+  const payLoad = JSON.stringify({
     "item": "barbeque"
   })
 
-  helpers.makeRequest('PUT', '/api/cart', data, token.id, (statusCode, data) => {
+  helpers.makeRequest('PUT', '/api/cart', payLoad, token.id, (statusCode, data) => {
     assert.strictEqual(statusCode, 200)
     assert.strictEqual(Array.isArray(data), true)
     assert.strictEqual(data.length, 2)
@@ -303,9 +302,9 @@ api["DELETE /api/cart should delete item and return user's cart"] = done => {
   const callBack = () => console.log('hello')
   const token = crypto.createToken(callBack)('i@a.com')(crypto.createRandomString(20))
 
-  const data = JSON.stringify({})
+  const payLoad = JSON.stringify({})
 
-  helpers.makeRequest('DELETE', '/api/cart?item=barbeque', data, token.id, (statusCode, data) => {
+  helpers.makeRequest('DELETE', '/api/cart?item=barbeque', payLoad, token.id, (statusCode, data) => {
     assert.strictEqual(statusCode, 200)
     assert.strictEqual(Array.isArray(data), true)
     assert.strictEqual(data.length, 1)
@@ -340,9 +339,9 @@ api['POST /api/order should order items in cart and return an array of orders'] 
   const callBack = () => console.log('hello')
   const token = crypto.createToken(callBack)('j@a.com')(crypto.createRandomString(20))
 
-  const data = JSON.stringify({})
+  const payLoad = JSON.stringify({})
 
-  helpers.makeRequest('POST', '/api/order', data, token.id, (statusCode, data) => {
+  helpers.makeRequest('POST', '/api/order', payLoad, token.id, (statusCode, data) => {
     assert.strictEqual(statusCode, 200)
     assert.strictEqual(Array.isArray(data), true)
     assert.strictEqual(data.length, 1)
@@ -361,38 +360,128 @@ api['POST /api/order should order items in cart and return an array of orders'] 
 }
 
 // GET
-api['POST /api/order should order items in cart and return an array of orders'] = done => {
+api['GET /api/order should return order object'] = done => {
+  const orderId = '4qdf2sxbsy9ky89c3rs1'
+
   // Create user
   const user = {
-    email: 'j@a.com',
+    email: 'k@a.com',
     lastName: 'BBB',
     firstName: 'CCC',
     hashedPassword: crypto.hash('123456'),
     streetAddress: 'Dansoman',
-    cart: [{"name":"pepperoni","price":13.99}, {"name":"barbeque","price":17.79}]
+    cart: [],
+    orders: [
+        {
+            'orderId': orderId,
+            'totalPrice': 17.99
+        }
+    ]
   }
 
   io.writeUser(user)
 
+  // Create order
+  const order = {
+    id: orderId,
+    totalPrice: 17.99,
+    items: [
+      {
+        name: 'marinara',
+        price: 17.99
+      }
+    ],
+    email: 'k@a.com'
+  }
+
+  io.writeFile(dir.orders(orderId), JSON.stringify(order))
+
   // Create token
   const callBack = () => console.log('hello')
-  const token = crypto.createToken(callBack)('j@a.com')(crypto.createRandomString(20))
+  const token = crypto.createToken(callBack)('k@a.com')(crypto.createRandomString(20))
 
-  const data = JSON.stringify({})
+  const payLoad = JSON.stringify({})
 
-  helpers.makeRequest('POST', '/api/order', data, token.id, (statusCode, data) => {
+  helpers.makeRequest(
+    'GET', '/api/order?id=4qdf2sxbsy9ky89c3rs1', payLoad, token.id, (statusCode, data) => {
     assert.strictEqual(statusCode, 200)
-    assert.strictEqual(Array.isArray(data), true)
-    assert.strictEqual(data.length, 1)
+    assert.strictEqual(typeof data, 'object')
 
     // Delete order
-    io.delete(dir.orders)(data[0]['orderId'])
+    io.delete(dir.orders)(orderId)
 
     // Delete token
     io.delete(dir.tokens)(token.id)
 
     // Delete user
-    io.delete(dir.users)('j@a.com')
+    io.delete(dir.users)('k@a.com')
+
+    done()
+  })
+}
+
+
+// checkout.js
+// POST
+api['POST /api/checkout should send email, payment and return success message'] = done => {
+  const orderId = '4qdf2sxbsy9ky89c3rs2'
+
+  // Create user
+  const user = {
+    email: 'l@a.com',
+    lastName: 'BBB',
+    firstName: 'CCC',
+    hashedPassword: crypto.hash('123456'),
+    streetAddress: 'Dansoman',
+    cart: [],
+    orders: [
+        {
+            'orderId': orderId,
+            'totalPrice': 17.99
+        }
+    ]
+  }
+
+  io.writeUser(user)
+
+  // Create order
+  const order = {
+    id: orderId,
+    totalPrice: 17.99,
+    items: [
+      {
+        name: 'marinara',
+        price: 17.99
+      }
+    ],
+    email: 'l@a.com'
+  }
+
+  io.writeFile(dir.orders(orderId), JSON.stringify(order))
+
+  // Create token
+  const callBack = () => console.log('hello')
+  const token = crypto.createToken(callBack)('l@a.com')(crypto.createRandomString(20))
+
+  const payLoad = JSON.stringify({
+    'stripeToken': 'tok_visa',
+    'orderId': orderId
+  })
+
+  helpers.makeRequest(
+    'POST', '/api/checkout', payLoad, token.id, (statusCode, data) => {
+    assert.strictEqual(statusCode, 200)
+    assert.strictEqual(typeof data, 'object')
+    assert.strictEqual(data['Success'], 'Payment processed and user notified successfully.')
+
+    // Delete order
+    io.delete(dir.orders)(orderId)
+
+    // Delete token
+    io.delete(dir.tokens)(token.id)
+
+    // Delete user
+    io.delete(dir.users)('l@a.com')
 
     done()
   })
